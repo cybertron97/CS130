a455f73a7776076736935858fd992869c1682eb1
Tanish Arora <taror002@wch133-18.cs.ucr.edu>
2019-02-11 20:52:45 -0800

    finished

--- lab4/lab-04-files/fragment.glsl
+++ lab4/lab-04-files/fragment.glsl
@@ -3,9 +3,20 @@ varying vec3 normal;
 varying vec3 light_direction;
 
 void main()
-{
+{   
     vec4 ambient = vec4(1, 0, 0, 1);
     vec4 diffuse = vec4(0, 1, 0, 1);
     vec4 specular = vec4(0, 0, 1, 1);
+    vec3 R = vec3(0,0,0);
+
+    ambient = gl_LightModel.ambient * gl_FrontMaterial.ambient * gl_LightSource[0].ambient;
+	
+    R = 2.0 * float(dot(light_direction,normal)) * normal - light_direction;
+    diffuse = gl_LightSource[0].diffuse * gl_FrontMaterial.diffuse * float(max(0.0,float(dot(normal,light_direction))));
+    vec4 reflected = vec4(R[0],R[1],R[2],0);
+    specular = gl_FrontMaterial.specular * float(pow(float(max(0.0,dot(-position,reflected))),gl_FrontMaterial.shininess));    
+
     gl_FragColor = ambient + diffuse + specular;
+
+
 }
